aa81bef0374c18459707fefed4fba4d34653a3f4
yumikobots <126863076+yumikobots@users.noreply.github.com>
2023-03-13 17:08:36 +0530

    Update __init__.py

--- santhuspam/modules/__init__.py
+++ santhuspam/modules/__init__.py
@@ -1,10 +1,32 @@
 from pyrogram import Client, filters
-from config import API_ID, API_HASH, STRING_SESSION
+from config.config import API_ID, API_HASH, STRING_SESSION
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
 from config import MONGO_DB_URL
-from spam.logger import LOGGER
+from santhuspam.logger import LOGGER
+import glob
+from os.path import dirname, isfile
+
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
+
+    all_modules = [
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f)
+        and f.endswith(".py")
+        and not f.endswith("__init__.py")
+    ]
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
+
 
 bot = Client(
     ":spambot:",
