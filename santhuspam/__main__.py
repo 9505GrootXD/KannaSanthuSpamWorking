0d7eb178910f9e7db6da84dd417ecac7dcfcdb9d
yumikobots <126863076+yumikobots@users.noreply.github.com>
2023-03-13 17:05:27 +0530

    Update __main__.py

--- santhuspam/__main__.py
+++ santhuspam/__main__.py
@@ -1,1 +1,24 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from santhuspam.modules import ALL_MODULES
+from santhuspam import clients, app, ids
 
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("santhuspam.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
