7e41538d92442d056721f59b361028150c91c989
yumikobots <126863076+yumikobots@users.noreply.github.com>
2023-03-15 19:54:53 +0530

    Update __init__.py

--- santhuspam/__init__.py
+++ santhuspam/__init__.py
@@ -48,5 +48,5 @@ bot = Client(
 
 if STRING_SESSION:
    print("Client: Found.. Starting..📳")
-   client = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="santhuspam/modules"))
+   client = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION, plugins=dict(root="santhuspam/modules"))
    clients.append(client)
