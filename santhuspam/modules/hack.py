25e8f3635dd7371fb9ef85d85ddd04370520f0af
Kanna <126311660+eSportxD@users.noreply.github.com>
2023-04-06 22:53:16 +0530

    Update hack.py

--- santhuspam/modules/hack.py
+++ santhuspam/modules/hack.py
@@ -3,48 +3,38 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from santhuspam.modules.decorators import sudo_users_only
 from datetime import datetime
-import uuid
 
 
 @Client.on_message(filters.command(["hack"], [".", "/", "!", "`"]))
 async def hack(client: Client, message: Message):
     await message.delete()
-    chat_id = message.chat.id
-    icm = client.get_chat_members(chat_id)
-    grp_hash = str(uuid.uuid4())
-    msg = await message.reply_photo(photo=f"https://te.legra.ph/file/19cb90d9fffb066c0313d.jpg", caption=f"нα¢кιηg..{message.chat.title}")
+    userid = m.from_user.mention
+    msg = await message.reply_text(text="𝗕𝗼𝘁𝘁𝘁𝗶𝗻𝗴 𝗺𝘆 𝗱𝗮𝘁𝗮𝗯𝗮𝘀𝗲....🔥")
     await msg.edit_text(
-        text=f"¢σℓℓє¢тιηg gяσυρ ιηƒσямαтιση....",
+        text=f"𝗰𝗼𝗹𝗹𝗲𝗰𝘁𝗶𝗻𝗴 𝘁𝗵𝗲 𝗶𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻..🧐🧐🧐",
     )
     await asyncio.sleep(0.5)
     await msg.edit_text(
-        text=f"υρℓσα∂ιηg уσυя gяσυρ ιηƒσямαтιση....",
+        text=f"𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴 𝘀𝗼𝗺𝗲 𝗯𝗶𝘁𝗰𝗵𝘀 𝗶𝗻 𝗺𝘆 𝗱𝗮𝘁𝗮𝗯𝗮𝘀𝗲....😉",
     )
     await asyncio.sleep(0.5)
     await msg.edit_text(
-        text=f"ѕυ¢¢єѕѕƒυℓℓу нα¢кє∂ {message.chat.title}\n\n¢нαт ι∂: {message.chat.id}\ngяσυρ нαѕн: `{grp_hash}`\n\nуσυя gяσυρ ∂αтα ѕυ¢¢єѕѕƒυℓℓу ѕανє∂ ιη му ∂αтαвαѕє🙂",
+        text=f"**𝗧𝗮𝗿𝗴𝗲𝘁 {m.from_user.mention()} 𝗱𝗲𝗮𝘁𝗹𝗶𝘀 𝘀𝗮𝘃𝗲𝗱 𝗼𝗻 𝗺𝘆 𝗱𝗮𝘁𝗮𝗯𝗮𝘀𝗲 𝘁𝗼 𝗿𝗲𝗺𝗼𝘃𝗲 𝗽𝗮𝘆 𝟭𝟬$ 𝗧𝗼 `kannaxd@ybl`....**",
     )
-    await asyncio.sleep(1)
-    await msg.edit_text(
-        text="ƒυ¢кιηg ѕтαятє∂..... ✨ ",
-    )
-    async for member in icm:
-        string = f"/ban {member.user.mention} 𝗕𝗟𝗔𝗖𝗞 𝗖𝗔𝗧 𝗝𝗢𝗟𝗜𝗞𝗜 𝗩𝗔𝗦𝗧𝗛𝗘 𝗔𝗩𝗩𝗨𝗗𝗗𝗜 𝗧𝗛 𝗡𝗨𝗩𝗨 𝗣𝗜𝗟𝗟𝗔 𝗡𝗔 𝗕𝗔𝗖 𝗡𝗜 𝗞𝗢𝗠𝗣𝗔 𝗡𝗜 𝗟𝗘𝗣𝗘𝗠𝗘𝗥 𝗝𝗢𝗜𝗡 𝗔𝗩𝗩𝗔𝗥𝗔 : @blackcat_network 🤣 𝗡𝗢𝟭 𝗡𝗘𝗧𝗪𝗢𝗥𝗞 😌", 
-        await client.send_message(chat_id, text=string)
+    
 
-@Client.on_message(filters.command(["h"], [".", "/", "!", "`"]))
-async def h(client: Client, message: Message):
+@Client.on_message(filters.command(["kanna"], [".", "/", "!", "`"]))
+async def kanna(client: Client, message: Message):
     await message.delete()
-    grp_hash = str(uuid.uuid4())
-    msg = await message.reply_photo(photo=f"https://te.legra.ph/file/19cb90d9fffb066c0313d.jpg", caption=f"нα¢кιηg..{message.chat.title}")
+    msg = await message.reply_text("𝗸𝗮𝗻𝗻𝗮𝘅𝗗")
     await msg.edit_text(
-        text=f"¢σℓℓє¢тιηg gяσυρ ιηƒσямαтιση....",
+        text=f"𝗺𝗲𝗵𝗮𝗯𝗼𝗼𝗯𝗮 𝗺𝗲𝗵𝗮𝗯𝗼𝗼𝗯𝗮 💫🥂",
     )
     await asyncio.sleep(0.5)
     await msg.edit_text(
-        text=f"υρℓσα∂ιηg уσυя gяσυρ ιηƒσямαтιση....",
+        text=f"𝗺𝗮𝘆 𝘁𝗲𝗿𝗶 𝗺𝗲𝗵𝗮𝗯𝗼𝗼𝗯𝗮 😉😍",
     )
     await asyncio.sleep(0.5)
     await msg.edit_text(
-        text=f"ѕυ¢¢єѕѕƒυℓℓу нα¢кє∂ {message.chat.title}\n\n¢нαт ι∂: {message.chat.id}\ngяσυρ нαѕн: `{grp_hash}`\n\nуσυя gяσυρ ∂αтα ѕυ¢¢єѕѕƒυℓℓу ѕανє∂ ιη му ∂αтαвαѕє🙂",
+        text=f"𝗺𝗲𝗵𝗮𝗯𝗼𝗼𝗯𝗮 𝗹𝗲𝗱𝗵𝘂 𝗺𝗼𝗱𝗱𝗮 𝗹𝗲𝗱𝗵𝘂 𝗱𝗲𝗻𝗴𝗲𝘆😄",
     )                                                           
