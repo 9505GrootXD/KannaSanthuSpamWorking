d7609839787ca41268f4354d3166245bb254344c
KaaL <99601120+KaalOS@users.noreply.github.com>
2023-04-17 13:03:55 +0530

    Update __init__.py

--- santhuspam/__init__.py
+++ santhuspam/__init__.py
@@ -47,5 +47,9 @@ bot = Client(
 
 if STRING_SESSION:
    print("Client: Found.. Starting..📳")
-   client = Client(api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION, plugins=dict(root="santhuspam/modules"))
+   client = Client(
+          api_id=API_ID,
+          api_hash=API_HASH,
+          string_session=STRING_SESSION,
+          plugins=dict(root="santhuspam/modules"))
    clients.append(client)
